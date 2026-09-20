0fe34b77b38de4491a5b39c1f50374c19efe7c41
anonynem07-web <anonynem07@gmail.com>
2026-09-20 18:26:05 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='alcove-v8',A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','fonts/bodoni.woff2','fonts/instr.woff2'];
+const V='alcove-v11',A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','fonts/bodoni.woff2','fonts/instr.woff2'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!=V).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(V).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
+self.addEventListener('fetch',e=>{if(e.request.method==='POST'&&new URL(e.request.url).pathname.endsWith('/share')){e.respondWith((async()=>{const fd=await e.request.formData(),f=fd.get('img'),c=await caches.open('share');if(f)await c.put('shared-img',new Response(f));return Response.redirect('./?share=1',303)})());return}if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(V).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
