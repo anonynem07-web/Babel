a67884d7f51136cb5fc2e3a214a0b36c6f72ec2c
anonynem07-web <anonynem07@gmail.com>
2026-09-20 16:46:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='alcove-v7',A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','fonts/bodoni.woff2','fonts/instr.woff2'];
+const V='alcove-v8',A=['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','fonts/bodoni.woff2','fonts/instr.woff2'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(V).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!=V).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(V).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
